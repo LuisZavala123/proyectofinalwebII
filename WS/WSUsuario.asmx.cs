9465a0caec492f2876922fb099aa2fdc7e74f33e
cuyo <cuyocosmico@gmail.com>
2020-05-28 13:16:44 -0500

    avia un } mal puesto en WSVenta

--- WS/WSUsuario.asmx.cs
+++ WS/WSUsuario.asmx.cs
@@ -18,7 +18,7 @@ namespace proyectofinalwebII.WS
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
-    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    
      [System.Web.Script.Services.ScriptService]
     public class WSUsuario : System.Web.Services.WebService
     {
